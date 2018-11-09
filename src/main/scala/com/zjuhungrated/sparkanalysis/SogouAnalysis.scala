3ee5a9691a063f09c890563e5f1185272355959f
hungrated <zhang295415658@qq.com>
2018-11-09 10:28:55 +0800

    master: add rdd operations

--- src/main/scala/com/zjuhungrated/sparkanalysis/SogouAnalysis.scala
+++ src/main/scala/com/zjuhungrated/sparkanalysis/SogouAnalysis.scala
@@ -16,14 +16,16 @@ object SogouAnalysis {
     //实例化configuration，用于编辑我们任务的相关信息
     val conf = new SparkConf().setAppName("SogouSparkAnalysis-Count").setMaster("local")
 
-    // sc是Spark Context，指的是“上下文”，也就是我们运行的环境，需要把conf当参数传进去
+    // sc是Spark Context，指的是“上下文”，也就是运行环境
     val sc = new SparkContext(conf)
 
     //通过sc获取一个（HDFS上的）文本文件
-    val raw = sc.textFile(INPUT_PATH).persist(StorageLevel.MEMORY_ONLY)
+    val raw = sc.textFile(INPUT_PATH)
 
     // 数据去空预处理
-    val rdd = raw.map(_.split("\t")).filter(_.length == 10)
+    val rdd = raw.map(_.split("\t"))
+      .filter(_.length == 10)
+      .persist(StorageLevel.MEMORY_ONLY)
 
     helper.deletDirIfExists(OUTPUT_PATH)
 
@@ -36,24 +38,27 @@ object SogouAnalysis {
     //      .sortByKey(false)
     //      .map(x => (x._2, x._1))
     //      .saveAsTextFile(OUTPUT_PATH + "/01")
-
     /**
-      * 2 求独立用户搜索关键词次数
-      */
-    // 用一下combine aggregate
-    //    rdd.map(x => ("sum", x(1).split("").length))
-    //      .reduceByKey((x, y) => x + y)
-    //      .map(x =>)
-    //      .saveAsTextFile(OUTPUT_PATH + "/02")
-    /**
-      * 3 比较各小时搜索量（OK）
+      * 2 比较各小时总搜索量（OK）
       */
     //    rdd.map(x => (x(9), 1))
     //      .reduceByKey((x, y) => x + y)
     //      .sortByKey()
     //      .saveAsTextFile(OUTPUT_PATH + "/03")
     /**
-      * 4 求独立用户搜索量并按此排序
+      * 3 比较各小时用户平均搜索量（OK）
+      */
+    //    rdd.map(x => (x(9), (x(1), 1)))
+    //      .combineByKey(
+    //      v => (v._2, 1),
+    //      (acc: (Int, Int), v) => (acc._1 + v._2, acc._2),
+    //      (acc1: (Int, Int), acc2: (Int, Int)) => (acc1._1 + acc2._1, acc1._2 + acc2._2)
+    //    )
+    //      .map { case (key, value) => (key, value._1 / value._2.toFloat) }
+    //      .sortByKey()
+    //      .saveAsTextFile(OUTPUT_PATH + "/03")
+    /**
+      * 4 求独立用户搜索量并按此排序（OK）
       */
     //    rdd.map(x => (x(1), 1))
     //      .reduceByKey((x, y) => x + y)
@@ -61,6 +66,7 @@ object SogouAnalysis {
     //      .sortByKey(false)
     //      .map(x => (x._2, x._1))
     //      .saveAsTextFile(OUTPUT_PATH + "/05")
+
     /**
       * 5 求用户点击序号与搜索结果排序均为1的URL并按点击次数排序（OK）
       */
@@ -75,7 +81,7 @@ object SogouAnalysis {
     /**
       * 6 求用户点击URL为搜索结果前10的占比
       */
-    // rdd
+    //     rdd
 
     sc.stop()
 
